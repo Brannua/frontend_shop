98d2fa0e6eb868067eb35463107f9b6ec1d96c07
Brannua <374453156@qq.com>
2020-02-14 15:54:14 +0800

    优化错误页面处理的用户体验

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <h1>404 页面走丢啦~</h1>
+  <div class="error">
+    <h2>404 页面走丢啦~</h2>
+    <van-button type="primary" @click="$router.push('/')">点击去往首页</van-button>
   </div>
 </template>
+
+<style lang="scss">
+.error {
+  text-align: center;
+  height: 3rem;
+  line-height: 3rem;
+}
+</style>
